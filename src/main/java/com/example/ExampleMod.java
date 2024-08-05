3f4452bd0a04cc28188610cfcbae674e9af44ad8
Positron <lxx11451@gmail.com>
2024-08-05 19:24:49 +0800

    Add Data Generation

--- src/main/java/com/example/ExampleMod.java
+++ src/main/java/com/example/ExampleMod.java
@@ -13,7 +13,7 @@ public class ExampleMod implements ModInitializer {
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final String modId = "tutorial";
-    public static final Logger LOGGER = LoggerFactory.getLogger("modid");
+    public static final Logger LOGGER = LoggerFactory.getLogger(modId);
 
 	@Override
 	public void onInitialize() {
